b3ba0ef2db10639f0d96f839899bf809a9324da6
Toma <230434@astanait.edu.kz>
2024-04-26 09:23:26 +0500

    MyList

---
+++ src/MyList.java
@@ -0,0 +1,19 @@
+public interface MyList <T>  extends Iterable <T> {
+    void add (T item);
+    void set ( int index ,T item);
+    void add (int index,T item);
+    T get (int index);
+    T gerFirst ();
+    T getLast();
+    void remove (int index );
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf (Object object);
+    int lastIndexOf (Object object);
+    boolean exists (Object object);
+    public Object[] toArray();
+    void clear();
+    int size ();
+
+}
