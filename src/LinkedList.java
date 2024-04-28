4079f0ce1f3fc17d6fea51b087712ddcb1148a7a
Toma <230434@astanait.edu.kz>
2024-04-29 00:15:42 +0500

    LinkedList with documentation

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,30 +1,42 @@
+/**
+ * @brief A generic doubly linked list implementation.
+ */
 public class MyLinkedList<T> implements MyList<T> {
     private static class MyNode<E> {
         E element;
         MyNode<E> next;
         MyNode<E> prev;
+
         MyNode(E element, MyNode<E> prev, MyNode<E> next) {
             this.element = element;
             this.prev = prev;
             this.next = next;
         }
     }
+
     private MyNode<T> head;
     private MyNode<T> tail;
     private int size;
 
+    /**
+     * @brief Constructs an empty linked list.
+     */
     public MyLinkedList() {
         head = null;
         tail = null;
         size = 0;
     }
 
-
-
+    /**
+     * @brief Retrieves a node at a given index.
+     * @param index The index of the node.
+     * @return The node at the specified index.
+     */
     private MyNode<T> getNode(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
+
         MyNode<T> current;
         if (index < size / 2) {
             current = head;
@@ -37,50 +49,73 @@ public class MyLinkedList<T> implements MyList<T> {
                 current = current.prev;
             }
         }
+
         return current;
     }
 
-    private void disable(MyNode<T> node) {
-        MyNode<T> pred = node.prev;
-        MyNode<T> succ = node.next;
+    /**
+     * @brief Disconnects a node from the list.
+     * @param node The node to unlink from the list.
+     */
+    private void unlink(MyNode<T> node) {
+        MyNode<T> prev = node.prev;
+        MyNode<T> next = node.next;
 
-        if (pred == null) {
-            head = succ;
+        if (prev == null) {
+            head = next;
         } else {
-            pred.next = succ;
-            node.prev = null;
+            prev.next = next;
         }
 
-        if (succ == null) {
-            tail = pred;
+        if (next == null) {
+            tail = prev;
         } else {
-            succ.prev = pred;
-            node.next = null;
+            next.prev = prev;
         }
+
 
         node.element = null;
+        node.next = null;
+        node.prev = null;
+
         size--;
     }
 
-
-    private boolean isEmpty() {
+    /**
+     * @brief Checks if the linked list is empty.
+     * @return true if the list has no elements, otherwise false.
+     */
+    public boolean isEmpty() {
         return size == 0;
     }
 
-
-
+    /**
+     * @brief Adds an item to the end of the list.
+     * @param item The item to add.
+     */
     public void add(T item) {
         addLast(item);
     }
 
+    /**
+     * @brief Sets the value of the item at a specific index.
+     * @param index The index to set the item at.
+     * @param item The new item.
+     */
     public void set(int index, T item) {
         getNode(index).element = item;
     }
 
+    /**
+     * @brief Inserts an item at a specified index.
+     * @param index The index to insert the item at.
+     * @param item The item to insert.
+     */
     public void add(int index, T item) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
+
         if (index == size) {
             addLast(item);
         } else if (index == 0) {
@@ -89,12 +124,18 @@ public class MyLinkedList<T> implements MyList<T> {
             MyNode<T> succ = getNode(index);
             MyNode<T> pred = succ.prev;
             MyNode<T> newNode = new MyNode<>(item, pred, succ);
-            succ.prev = newNode;
+
             pred.next = newNode;
+            succ.prev = newNode;
+
             size++;
         }
     }
 
+    /**
+     * @brief Inserts an item at the beginning of the list.
+     * @param item The item to insert.
+     */
     public void addFirst(T item) {
         if (isEmpty()) {
             head = tail = new MyNode<>(item, null, null);
@@ -103,9 +144,14 @@ public class MyLinkedList<T> implements MyList<T> {
             head.prev = newNode;
             head = newNode;
         }
+
         size++;
     }
 
+    /**
+     * @brief Inserts an item at the end of the list.
+     * @param item The item to insert.
+     */
     public void addLast(T item) {
         if (isEmpty()) {
             head = tail = new MyNode<>(item, null, null);
@@ -114,13 +160,23 @@ public class MyLinkedList<T> implements MyList<T> {
             tail.next = newNode;
             tail = newNode;
         }
+
         size++;
     }
 
+    /**
+     * @brief Retrieves the item at a specific index.
+     * @param index The index to get the item from.
+     * @return The item at the specified index.
+     */
     public T get(int index) {
         return getNode(index).element;
     }
 
+    /**
+     * @brief Gets the first item in the list.
+     * @return The first item.
+     */
     public T getFirst() {
         if (isEmpty()) {
             throw new IllegalStateException("List is empty");
@@ -128,6 +184,10 @@ public class MyLinkedList<T> implements MyList<T> {
         return head.element;
     }
 
+    /**
+     * @brief Gets the last item in the list.
+     * @return The last item.
+     */
     public T getLast() {
         if (isEmpty()) {
             throw new IllegalStateException("List is empty");
@@ -135,58 +195,51 @@ public class MyLinkedList<T> implements MyList<T> {
         return tail.element;
     }
 
+    /**
+     * @brief Removes an item from a specific index.
+     * @param index The index to remove the item from.
+     */
     public void remove(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
         MyNode<T> nodeToRemove = getNode(index);
-        disable(nodeToRemove);
+        unlink(nodeToRemove);
     }
 
+    /**
+     * @brief Removes the first item in the list.
+     */
     public void removeFirst() {
         if (isEmpty()) {
             throw new IllegalStateException("List is empty");
         }
-        disable(head);
+        unlink(head);
     }
 
+    /**
+     * @brief Removes the last item in the list.
+     */
     public void removeLast() {
         if (isEmpty()) {
             throw new IllegalStateException("List is empty");
         }
-        disable(tail);
+        unlink(tail);
     }
 
-    public void sort() {
-        throw new UnsupportedOperationException("Sorting is not supported for linked list");
-    }
-
-    public int indexOf(Object object) {
-        int index = 0;
-        for (MyNode<T> current = head; current != null; current = current.next) {
-            if (object.equals(current.element)) {
-                return index;
-            }
-            index++;
-        }
-        return -1;
-    }
-
-    public int lastIndexOf(Object object) {
-        int index = size - 1;
-        for (MyNode<T> current = tail; current != null; current = current.prev) {
-            if (object.equals(current.element)) {
-                return index;
-            }
-            index--;
-        }
-        return -1;
-    }
-
+    /**
+     * @brief Checks if an object exists in the list.
+     * @param object The object to search for.
+     * @return true if the object is found, otherwise false.
+     */
     public boolean exists(Object object) {
         return indexOf(object) != -1;
     }
 
+    /**
+     * @brief Converts the list to an array.
+     * @return An array containing all elements in the list.
+     */
     public Object[] toArray() {
         Object[] array = new Object[size];
         int index = 0;
@@ -196,6 +249,9 @@ public class MyLinkedList<T> implements MyList<T> {
         return array;
     }
 
+    /**
+     * @brief Clears all elements from the list.
+     */
     public void clear() {
         while (head != null) {
             MyNode<T> next = head.next;
@@ -207,6 +263,10 @@ public class MyLinkedList<T> implements MyList<T> {
         size = 0;
     }
 
+    /**
+     * @brief Returns the current size of the list.
+     * @return The number of elements in the list.
+     */
     public int size() {
         return size;
     }
